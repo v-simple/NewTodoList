7315d44f39204f619d978f2a240f208a84df8383
김용일 <kyi910705@gmail.com>
2022-01-10 18:21:40 +0900

    체크박스 기능 추가

--- js/index.js
+++ js/index.js
@@ -4,48 +4,48 @@ const div = document.getElementById('list')
 
 //엔터로 입력시 ?????   그리고 input 입력시 비어있을때 입력 안되게 하기???
 input.addEventListener('keyup', (e) => {
-    if(e.keyCode === 13){
-        const List = document.createElement('li')  
-        //리스트 생성
-        List.innerHTML = input.value;
-        div.appendChild(List)
-        //input 다시 공백으로
-        input.value = ''
-        
-        //리스트 가로줄 긋기
-        List.addEventListener('click', () => {
-            List.style.textDecoration = 'line-through'
-        } );
-        
-        //리스트 삭제
-        const deletebutton =  document.createElement('button')
-        deletebutton.textContent = '제거하기'
-        deletebutton.addEventListener('click', () => {
-            List.parentNode.removeChild(List)
-        })
-        List.appendChild(deletebutton)
-    }
+  if(e.keyCode === 13){
+    addList()
+  }
 })
 
 addbtn.addEventListener('click', () => {
-  const List = document.createElement('li') 
-  //리스트 생성
-  List.innerHTML = input.value;
+  addList()
+})
+
+function addList() {
+  if (input.value === '') return
+  
+  const List = document.createElement('div')
+
+  const Input = document.createElement('input')
+
+  const Label = document.createElement('label')
+  Label.innerText = input.value
+  Label.style.marginLeft = '10px'
+
+  Input.type = 'checkbox'
+  Input.addEventListener('change', (e) => {
+      if (Input.checked) {
+      Label.style.textDecoration = 'line-through'
+      } else {
+      Label.style.textDecoration = 'none'
+      }
+  })
+
+  //리스트 삭제
+  const DeleteButton =  document.createElement('button')
+  DeleteButton.textContent = '제거하기'
+  DeleteButton.style.marginLeft = '10px'
+  DeleteButton.addEventListener('click', () => {
+      List.parentNode.removeChild(List)
+  })
+
+  List.appendChild(Input)
+  List.appendChild(Label)
+  List.appendChild(DeleteButton)
+
   div.appendChild(List)
   //input 다시 공백으로
   input.value = ''
-
-  //리스트 가로줄 긋기
-  List.addEventListener('click', () => {
-    List.style.textDecoration = 'line-through'
-  } );
-
-  //리스트 삭제
-  const deletebutton =  document.createElement('button')
-  deletebutton.textContent = '제거하기'
-  deletebutton.addEventListener('click', () => {
-    List.parentNode.removeChild(List)
-  })
-  List.appendChild(deletebutton)
-
-})
+}
